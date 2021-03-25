1fc4fb4842a67939f21ff70da0adf5326fb93f05
q4speed <kun@fit2cloud.com>
2021-03-25 15:35:41 +0800

    fix(示例): 修复form不能校验的bug

--- src/business/components/CreateFormDemo.vue
+++ src/business/components/CreateFormDemo.vue
@@ -1,17 +1,17 @@
 <template>
   <layout-content header="创建表单" back-name="ComplexTableDemo">
     <el-form ref="form" :model="form" :rules="rules" label-width="auto" label-position="right">
-      <el-form-item label="活动名称">
+      <el-form-item label="活动名称" prop="name">
         <el-input v-model="form.name"></el-input>
       </el-form-item>
-      <el-form-item label="活动区域">
+      <el-form-item label="活动区域" prop="region">
         <el-select v-model="form.region" placeholder="请选择活动区域">
           <el-option label="区域一" value="shanghai"></el-option>
           <el-option label="区域二" value="beijing"></el-option>
         </el-select>
       </el-form-item>
 
-      <el-form-item label="活动形式">
+      <el-form-item label="活动形式" prop="desc">
         <el-input type="textarea" v-model="form.desc"></el-input>
       </el-form-item>
       <el-form-item>
