7b5d2c641d37036ebc903fcbb9a394fa4fcbd945
wangdan <dan.wang@fit2cloud.com>
2021-11-30 14:43:08 +0800

    feat: 打包体积优化

---
+++ src/main-prod.js
@@ -0,0 +1,41 @@
+import Vue from "vue"
+import "@/styles/index.scss"
+import Fit2CloudUI from "fit2cloud-ui";
+// import ElementUI from "element-ui";
+import App from "./App.vue"
+import i18n from "./i18n";
+import router from "./router"
+import store from "./store"
+import icons from "./icons"
+import plugins from "./plugins";
+import directives from "./directive";
+import filters from "./filters";
+import "./permission"
+
+Vue.config.productionTip = false
+
+// 仅用于demo，项目中去掉这段代码
+if (process.env.NODE_ENV === 'demo') {
+  const {mockXHR} = require('../mock')
+  mockXHR()
+}
+
+// Vue.use(ElementUI, {
+//   size: "small",
+//   i18n: (key, value) => i18n.t(key, value)
+// });
+Vue.use(Fit2CloudUI, {
+  i18n: (key, value) => i18n.t(key, value)
+});
+Vue.use(icons);
+Vue.use(plugins);
+Vue.use(directives);
+Vue.use(filters);
+
+new Vue({
+  el: "#app",
+  i18n,
+  router,
+  store,
+  render: h => h(App),
+})
--- vue.config.js
+++ vue.config.js
@@ -24,5 +24,27 @@ module.exports = {
         '@': resolve('src')
       }
     }
-  }
+  },
+  chainWebpack: config => {
+    // 发布模式
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config
+        .entry('app')
+        .clear()
+        .add('./src/main-prod.js')
+      // 通过externals加载外部CDN资源
+      config.set('externals', {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        "element-ui": "ELEMENT"
+      })
+    })
+    // 开发模式
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config
+        .entry('app')
+        .clear()
+        .add('./src/main.js')
+    })
+  },
 };
